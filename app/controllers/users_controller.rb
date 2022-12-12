9e89d62bd7ab333e84da5feb059b7b39f16e2fec
michael-duke <michael.21duke@gmail.com>
2022-12-12 16:38:19 +0300

    Fix Rubocop linter errors according to suggestions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   end
 
   private
+
   # Use callbacks to share common setup or constraints between actions.
   def set_user
     @user = User.find(params[:id])
