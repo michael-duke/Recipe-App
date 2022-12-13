83e52631d789d27c536d484579c4e0592dba4ea8
samiullah997 <samiullahk997@gmail.com>
2022-12-13 15:06:59 +0500

    Update foods controller to set before_action for set_user method

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,5 +1,6 @@
 class FoodsController < ApplicationController
   before_action :set_food, only: %i[show destroy]
+  before_action :set_user, only: %i[index show create]
 
   def index
     @foods = @user.foods
