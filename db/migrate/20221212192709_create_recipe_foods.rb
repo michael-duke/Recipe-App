dc66ff4463be502f7af5fb100628d4aac3f143ac
michael-duke <michael.21duke@gmail.com>
2022-12-13 15:39:47 +0300

    Generate new migrations based on the changes in the models - Generate schema on changes made on recipe_food many to many relationship

--- db/migrate/20221212192709_create_recipe_foods.rb
+++ db/migrate/20221212192709_create_recipe_foods.rb
@@ -1,7 +1,7 @@
 class CreateRecipeFoods < ActiveRecord::Migration[7.0]
   def change
     create_table :recipe_foods do |t|
-      t.references :user, null: false, foreign_key: true
+      t.references :recipe, null: false, foreign_key: true
       t.references :food, null: false, foreign_key: true
       t.string :quantity
 
--- db/schema.rb
+++ db/schema.rb
@@ -26,13 +26,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_12_192709) do
   end
 
   create_table "recipe_foods", force: :cascade do |t|
-    t.bigint "user_id", null: false
+    t.bigint "recipe_id", null: false
     t.bigint "food_id", null: false
     t.string "quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["food_id"], name: "index_recipe_foods_on_food_id"
-    t.index ["user_id"], name: "index_recipe_foods_on_user_id"
+    t.index ["recipe_id"], name: "index_recipe_foods_on_recipe_id"
   end
 
   create_table "recipes", force: :cascade do |t|
@@ -55,6 +55,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_12_192709) do
 
   add_foreign_key "foods", "users"
   add_foreign_key "recipe_foods", "foods"
-  add_foreign_key "recipe_foods", "users"
+  add_foreign_key "recipe_foods", "recipes"
   add_foreign_key "recipes", "users"
 end
