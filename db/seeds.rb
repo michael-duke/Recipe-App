dae7cc3ff05751c0718bcdaf150cdefbdde10854
samiullah997 <samiullahk997@gmail.com>
2022-12-14 18:29:51 +0500

    add additional data to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,5 +15,20 @@ foods = Food.create([
     {user_id: 1, name: 'Potatoe', measurement_unit: 'grams', price: 3, quantity: 15},
     {user_id: 2, name: 'Cheery', measurement_unit: 'grams', price: 5, quantity: 20},
     {user_id: 1, name: 'Carrot', measurement_unit: 'grams', price: 4, quantity: 30},
-    {user_id: 2, name: 'Flour', measurement_unit: 'kg', price: 14, quantity: 84}
+    {user_id: 2, name: 'Flour', measurement_unit: 'kg', price: 14, quantity: 84},
+    {user_id: 1, name: 'Milk', measurement_unit: 'ml', price: 2, quantity: 100},
+    {user_id: 2, name: 'Sugar', measurement_unit: 'grams', price: 3, quantity: 50},
+    {user_id: 1, name: 'Egg', measurement_unit: 'pieces', price: 1, quantity: 10},
+    {user_id: 2, name: 'Bread', measurement_unit: 'pieces', price: 2, quantity: 20},
+    {user_id: 1, name: 'Butter', measurement_unit: 'grams', price: 4, quantity: 50},
+    {user_id: 2, name: 'Salt', measurement_unit: 'grams', price: 1, quantity: 100},
+])
+
+recipes = Recipe.create([
+    {user_id: 1, name: 'Potatoe Salad', description: 'A delicious potatoe salad', preparation_time: 10, cooking_time: 20, public: true},
+    {user_id: 2, name: 'Cheery Pie', description: 'A delicious cheery pie', preparation_time: 20, cooking_time: 30, public: true},
+    {user_id: 1, name: 'Carrot Cake', description: 'A delicious carrot cake', preparation_time: 30, cooking_time: 40, public: false},
+    {user_id: 2, name: 'Flour Bread', description: 'A delicious flour bread', preparation_time: 40, cooking_time: 50, public: true},
+    {user_id: 1, name: 'Milk Shake', description: 'A delicious milk shake', preparation_time: 50, cooking_time: 60, public: true},
+    {user_id: 2, name: 'Sugar Cookies', description: 'A delicious sugar cookies', preparation_time: 60, cooking_time: 70, public: false},
 ])
