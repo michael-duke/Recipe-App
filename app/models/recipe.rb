73707e2e5188b0d8a2db144ab11a0e594ead1e82
michael-duke <michael.21duke@gmail.com>
2022-12-14 11:22:23 +0300

    Implement validations for food, recipe, recipe_food, and user models

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,9 @@
 class Recipe < ApplicationRecord
+  validates :name, presence: true, length: { in: 1..18 }
+  validates :preparation_time, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :cooking_time, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :description, presence: true, length: { in: 1..450 }
+
   belongs_to :user
   has_many :recipe_foods, dependent: :destroy
 end
