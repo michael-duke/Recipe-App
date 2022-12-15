95e73113e31a3ac273da87700f328d1b083509fc
michael-duke <michael.21duke@gmail.com>
2022-12-15 16:56:43 +0300

    Implement the before_action method for obtaining the current user and recipe - Use select to filter the ingredients that are needed for shopping - Calculate the total cost of the missing ingredients

--- app/controllers/general_shopping_list_controller.rb
+++ app/controllers/general_shopping_list_controller.rb
@@ -1,3 +1,31 @@
 class GeneralShoppingListController < ApplicationController
-  def index; end
+  before_action :set_recipe, only: [:index]
+  before_action :set_user, only: [:index]
+
+  def index
+    @recipe_foods = @recipe.recipe_foods.includes(:food).order(:id)
+
+    @missing_ingredients = @recipe_foods.select { |ingredient| ingredient.quantity > ingredient.food.quantity }
+    @missing_ingredients = @missing_ingredients.map do |ingredient|
+      {
+        name: ingredient.food.name,
+        unit: ingredient.food.measurement_unit,
+        quantity: ingredient.quantity - ingredient.food.quantity,
+        price: ingredient.food.price * (ingredient.quantity - ingredient.food.quantity)
+      }
+    end
+
+    @total_price = @missing_ingredients.map { |ingredient| ingredient[:price] }.sum
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+
+  def set_recipe
+    @recipe = set_user.recipes.find(params[:recipe_id])
+  end
 end
