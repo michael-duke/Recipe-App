b840c81d8f6ad86a4c660bb851c8a0c66188110a
samiullah997 <samiullahk997@gmail.com>
2022-12-16 14:27:25 +0500

    Implement controller testing for users and recipes

---
+++ spec/requests/users_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe 'Users', type: :request do
+  before :each do
+    users = User.create([
+      { name: 'Samiullah', email: 'samiullahk997@gmail.com', password: 'samiullahk997' },
+      { name: 'Michael', email: 'michael@rebels.com', password: 'michael'}
+    ])
+    
+    get users_path
+  end
+
+  it "assigns all users to @users" do
+   expect(assigns(:users)).to eq(User.all.order(:id))
+  end
+
+  it "is a success" do
+    expect(response).to have_http_status(:ok)
+  end
+  it "renders 'index' template" do
+    expect(response).to render_template('index')
+  end
+end
