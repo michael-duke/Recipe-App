072482315bd583ac82a48f36a3ca2c0890855503
michael-duke <michael.21duke@gmail.com>
2023-02-03 12:55:08 +0300

    Install @hotwired/turbo-rails and @hotwired/stimulus Remove tailwind.config.js from javascript folder Add Material tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,24 @@
-module.exports = {
+const withMT = require("@material-tailwind/html/utils/withMT");
+
+module.exports = withMT ({
   content: [
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
-  ]
-}
+  ],
+  theme: {
+    extend: {
+      colors: {
+        clifford: '#da373d',
+        darkSlate: '#1e293b'
+      },
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-3deg)' },
+          '50%': { transform: 'rotate(3deg)' },
+        }
+      },
+    },
+  },
+});
